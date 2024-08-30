689d020ce26b6a8ac593873897878d87af85f9af
Yasar Cakir <dev.yasarcakir@gmail.com>
2024-08-30 16:12:05 +0700

    Added translation field and refactored queries

--- internals/models/country.go
+++ internals/models/country.go
@@ -2,174 +2,195 @@ package models
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
+type QueryCountry struct {
+	ID   primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	Name struct {
+		Common   string `json:"common" bson:"common,omitempty"`
+		Official string `json:"official,omitempty" bson:"official,omitempty"`
+	} `json:"name,omitempty" bson:"name,omitempty"`
+	Cca2  string `json:"cca2" bson:"cca2"`
+	Flag  string `json:"flag,omitempty" bson:"flag,omitempty"`
+	Flags struct {
+		Svg string `json:"svg,omitempty" bson:"svg,omitempty"`
+		Png string `json:"png,omitempty" bson:"png,omitempty"`
+		Alt string `json:"alt,omitempty" bson:"alt,omitempty"`
+	} `json:"flags,omitempty" bson:"flags,omitempty"`
+	Translations struct {
+		Tur struct {
+			Official string `json:"official,omitempty" bson:"official,omitempty"`
+			Common   string `json:"common,omitempty" bson:"common,omitempty"`
+		} `json:"tur,omitempty" bson:"tur,omitempty"`
+	} `json:"translations" bson:"translations"`
+}
+
 type Country struct {
 	ID   primitive.ObjectID `json:"id" bson:"_id,omitempty"`
 	Name struct {
-		Common     string `bson:"common,omitempty"`
-		Official   string `bson:"official,omitempty"`
+		Common     string `json:"common" bson:"common,omitempty"`
+		Official   string `json:"official,omitempty" bson:"official,omitempty"`
 		NativeName struct {
 			Nld struct {
-				Official string `bson:"official,omitempty"`
-				Common   string `bson:"common,omitempty"`
-			} `bson:"nld,omitempty"`
+				Official string `json:"official,omitempty" bson:"official,omitempty"`
+				Common   string `json:"common,omitempty" bson:"common,omitempty"`
+			} `json:"nld,omitempty" bson:"nld,omitempty"`
 			Pap struct {
-				Official string `bson:"official,omitempty"`
-				Common   string `bson:"common,omitempty"`
-			} `bson:"pap,omitempty"`
-		} `bson:"nativeName,omitempty"`
-	} `bson:"name,omitempty"`
-	Tld         []string `json:"tld" bson:"tld,omitempty"`
-	Cca2        string   `bson:"cca2"`
-	Ccn3        string   `bson:"ccn3,omitempty"`
-	Cca3        string   `bson:"cca3,omitempty"`
-	Cioc        string   `bson:"cioc,omitempty"`
-	Fifa        string   `bson:"fifa,omitempty"`
-	Independent bool     `bson:"independent,omitempty"`
-	Status      string   `bson:"status,omitempty"`
-	UnMember    bool     `bson:"unMember,omitempty"`
+				Official string `json:"official,omitempty" bson:"official,omitempty"`
+				Common   string `json:"common,omitempty" bson:"common,omitempty"`
+			} `json:"pap,omitempty" bson:"pap,omitempty"`
+		} `json:"nativeName,omitempty" bson:"nativeName,omitempty"`
+	} `json:"name,omitempty" bson:"name,omitempty"`
+	Tld         []string `json:"tld,omitempty" bson:"tld,omitempty"`
+	Cca2        string   `json:"cca2" bson:"cca2"`
+	Ccn3        string   `json:"ccn3,omitempty" bson:"ccn3,omitempty"`
+	Cca3        string   `json:"cca3,omitempty" bson:"cca3,omitempty"`
+	Cioc        string   `json:"cioc,omitempty" bson:"cioc,omitempty"`
+	Fifa        string   `json:"fifa,omitempty" bson:"fifa,omitempty"`
+	Independent bool     `json:"independent,omitempty" bson:"independent,omitempty"`
+	Status      string   `json:"status,omitempty" bson:"status,omitempty"`
+	UnMember    bool     `json:"unMember,omitempty" bson:"unMember,omitempty"`
 	Idd         struct {
-		Root     string   `bson:"root,omitempty"`
-		Suffixes []string `bson:"suffixes,omitempty"`
-	} `bson:"idd,omitempty"`
-	Capital     []string `bson:"capital,omitempty"`
+		Root     string   `json:"root,omitempty" bson:"root,omitempty"`
+		Suffixes []string `json:"suffixes,omitempty" bson:"suffixes,omitempty"`
+	} `json:"idd,omitempty" bson:"idd,omitempty"`
+	Capital     []string `json:"capital,omitempty" bson:"capital,omitempty"`
 	CapitalInfo struct {
-		Latlng []float64 `bson:"latlng,truncate,omitempty"`
-	} `bson:"capitalInfo,omitempty"`
-	AltSpellings []string `bson:"altSpellings,omitempty"`
-	Region       string   `bson:"region,omitempty"`
-	Subregion    string   `bson:"subregion,omitempty"`
-	Continents   []string `bson:"continents,omitempty"`
+		Latlng []float64 `json:"latlng,omitempty" bson:"latlng,truncate,omitempty"`
+	} `json:"capitalInfo,omitempty" bson:"capitalInfo,omitempty"`
+	AltSpellings []string `json:"altSpellings,omitempty" bson:"altSpellings,omitempty"`
+	Region       string   `json:"region,omitempty" bson:"region,omitempty"`
+	Subregion    string   `json:"subregion,omitempty" bson:"subregion,omitempty"`
+	Continents   []string `json:"continents,omitempty" bson:"continents,omitempty"`
 	Languages    struct {
-		Nld string `bson:"nld,omitempty"`
-		Pap string `bson:"pap,omitempty"`
-	} `bson:"languages,omitempty"`
+		Nld string `json:"nld,omitempty" bson:"nld,omitempty"`
+		Pap string `json:"pap,omitempty" bson:"pap,omitempty"`
+	} `json:"languages,omitempty" bson:"languages,omitempty"`
 	Translations struct {
 		Ara struct {
-			Official string `bson:"official,omitempty"`
-			Common   string `bson:"common"`
-		} `bson:"ara"`
+			Official string `json:"official,omitempty" bson:"official,omitempty"`
+			Common   string `json:"common,omitempty" bson:"common,omitempty"`
+		} `json:"ara,omitempty" bson:"ara,omitempty"`
 		Bre struct {
-			Official string `bson:"official,omitempty"`
-			Common   string `bson:"common"`
-		} `bson:"bre"`
+			Official string `json:"official,omitempty" bson:"official,omitempty"`
+			Common   string `json:"common,omitempty" bson:"common,omitempty"`
+		} `json:"bre,omitempty" bson:"bre,omitempty"`
 		Ces struct {
-			Official string `bson:"official,omitempty"`
-			Common   string `bson:"common"`
-		} `bson:"ces"`
+			Official string `json:"official,omitempty" bson:"official,omitempty"`
+			Common   string `json:"common,omitempty" bson:"common,omitempty"`
+		} `json:"ces,omitempty" bson:"ces,omitempty"`
 		Cym struct {
-			Official string `bson:"official,omitempty"`
-			Common   string `bson:"common"`
-		} `bson:"cym"`
+			Official string `json:"official,omitempty" bson:"official,omitempty"`
+			Common   string `json:"common,omitempty" bson:"common,omitempty"`
+		} `json:"cym,omitempty" bson:"cym,omitempty"`
 		Deu struct {
-			Official string `bson:"official,omitempty"`
-			Common   string `bson:"common"`
-		} `bson:"deu"`
+			Official string `json:"official,omitempty" bson:"official,omitempty"`
+			Common   string `json:"common,omitempty" bson:"common,omitempty"`
+		} `json:"deu,omitempty" bson:"deu,omitempty"`
 		Est struct {
-			Official string `bson:"official,omitempty"`
-			Common   string `bson:"common"`
-		} `bson:"est"`
+			Official string `json:"official,omitempty" bson:"official,omitempty"`
+			Common   string `json:"common,omitempty" bson:"common,omitempty"`
+		} `json:"est,omitempty" bson:"est,omitempty"`
 		Fin struct {
-			Official string `bson:"official,omitempty"`
-			Common   string `bson:"common"`
-		} `bson:"fin"`
+			Official string `json:"official,omitempty" bson:"official,omitempty"`
+			Common   string `json:"common,omitempty" bson:"common,omitempty"`
+		} `json:"fin,omitempty" bson:"fin,omitempty"`
 		Fra struct {
-			Official string `bson:"official,omitempty"`
-			Common   string `bson:"common"`
-		} `bson:"fra"`
+			Official string `json:"official,omitempty" bson:"official,omitempty"`
+			Common   string `json:"common,omitempty" bson:"common,omitempty"`
+		} `json:"fra,omitempty" bson:"fra,omitempty"`
 		Hrv struct {
-			Official string `bson:"official,omitempty"`
-			Common   string `bson:"common"`
-		} `bson:"hrv"`
+			Official string `json:"official,omitempty" bson:"official,omitempty"`
+			Common   string `json:"common,omitempty" bson:"common,omitempty"`
+		} `json:"hrv,omitempty" bson:"hrv,omitempty"`
 		Hun struct {
-			Official string `bson:"official,omitempty"`
-			Common   string `bson:"common"`
-		} `bson:"hun"`
+			Official string `json:"official,omitempty" bson:"official,omitempty"`
+			Common   string `json:"common,omitempty" bson:"common,omitempty"`
+		} `json:"hun,omitempty" bson:"hun,omitempty"`
 		Ita struct {
-			Official string `bson:"official,omitempty"`
-			Common   string `bson:"common"`
-		} `bson:"ita"`
+			Official string `json:"official,omitempty" bson:"official,omitempty"`
+			Common   string `json:"common,omitempty" bson:"common,omitempty"`
+		} `json:"ita,omitempty" bson:"ita,omitempty"`
 		Jpn struct {
-			Official string `bson:"official,omitempty"`
-			Common   string `bson:"common"`
-		} `bson:"jpn"`
+			Official string `json:"official,omitempty" bson:"official,omitempty"`
+			Common   string `json:"common,omitempty" bson:"common,omitempty"`
+		} `json:"jpn,omitempty" bson:"jpn,omitempty"`
 		Kor struct {
-			Official string `bson:"official,omitempty"`
-			Common   string `bson:"common"`
-		} `bson:"kor"`
+			Official string `json:"official,omitempty" bson:"official,omitempty"`
+			Common   string `json:"common,omitempty" bson:"common,omitempty"`
+		} `json:"kor,omitempty" bson:"kor,omitempty"`
 		Nld struct {
-			Official string `bson:"official,omitempty"`
-			Common   string `bson:"common"`
-		} `bson:"nld"`
+			Official string `json:"official,omitempty" bson:"official,omitempty"`
+			Common   string `json:"common,omitempty" bson:"common,omitempty"`
+		} `json:"nld,omitempty" bson:"nld,omitempty"`
 		Per struct {
-			Official string `bson:"official,omitempty"`
-			Common   string `bson:"common"`
-		} `bson:"per"`
+			Official string `json:"official,omitempty" bson:"official,omitempty"`
+			Common   string `json:"common,omitempty" bson:"common,omitempty"`
+		} `json:"per,omitempty" bson:"per,omitempty"`
 		Pol struct {
-			Official string `bson:"official,omitempty"`
-			Common   string `bson:"common"`
-		} `bson:"pol"`
+			Official string `json:"official,omitempty" bson:"official,omitempty"`
+			Common   string `json:"common,omitempty" bson:"common,omitempty"`
+		} `json:"pol,omitempty" bson:"pol,omitempty"`
 		Por struct {
-			Official string `bson:"official,omitempty"`
-			Common   string `bson:"common"`
-		} `bson:"por"`
+			Official string `json:"official,omitempty" bson:"official,omitempty"`
+			Common   string `json:"common,omitempty" bson:"common,omitempty"`
+		} `json:"por,omitempty" bson:"por,omitempty"`
 		Rus struct {
-			Official string `bson:"official,omitempty"`
-			Common   string `bson:"common"`
-		} `bson:"rus"`
+			Official string `json:"official,omitempty" bson:"official,omitempty"`
+			Common   string `json:"common,omitempty" bson:"common,omitempty"`
+		} `json:"rus,omitempty" bson:"rus,omitempty"`
 		Slk struct {
-			Official string `bson:"official,omitempty"`
-			Common   string `bson:"common"`
-		} `bson:"slk"`
+			Official string `json:"official,omitempty" bson:"official,omitempty"`
+			Common   string `json:"common,omitempty" bson:"common,omitempty"`
+		} `json:"slk,omitempty" bson:"slk,omitempty"`
 		Spa struct {
-			Official string `bson:"official,omitempty"`
-			Common   string `bson:"common"`
-		} `bson:"spa"`
+			Official string `json:"official,omitempty" bson:"official,omitempty"`
+			Common   string `json:"common,omitempty" bson:"common,omitempty"`
+		} `json:"spa,omitempty" bson:"spa,omitempty"`
 		Srp struct {
-			Official string `bson:"official,omitempty"`
-			Common   string `bson:"common"`
-		} `bson:"srp"`
+			Official string `json:"official,omitempty" bson:"official,omitempty"`
+			Common   string `json:"common,omitempty" bson:"common,omitempty"`
+		} `json:"srp,omitempty" bson:"srp,omitempty"`
 		Swe struct {
-			Official string `bson:"official,omitempty"`
-			Common   string `bson:"common"`
-		} `bson:"swe"`
+			Official string `json:"official,omitempty" bson:"official,omitempty"`
+			Common   string `json:"common,omitempty" bson:"common,omitempty"`
+		} `json:"swe,omitempty" bson:"swe,omitempty"`
 		Tur struct {
-			Official string `bson:"official,omitempty"`
-			Common   string `bson:"common"`
-		} `bson:"tur"`
+			Official string `json:"official,omitempty" bson:"official,omitempty"`
+			Common   string `json:"common,omitempty" bson:"common,omitempty"`
+		} `json:"tur,omitempty" bson:"tur,omitempty"`
 		Urd struct {
-			Official string `bson:"official,omitempty"`
-			Common   string `bson:"common"`
-		} `bson:"urd"`
+			Official string `json:"official,omitempty" bson:"official,omitempty"`
+			Common   string `json:"common,omitempty" bson:"common,omitempty"`
+		} `json:"urd,omitempty" bson:"urd,omitempty"`
 		Zho struct {
-			Official string `bson:"official,omitempty"`
-			Common   string `bson:"common"`
-		} `bson:"zho"`
-	} `bson:"translations"`
-	Latlng     []float64 `bson:"latlng,truncate,omitempty"`
-	Landlocked bool      `bson:"landlocked,omitempty"`
-	Borders    []any     `bson:"borders,omitempty"`
-	Area       int       `bson:"area,truncate,omitempty"`
-	Flag       string    `bson:"flag"`
+			Official string `json:"official,omitempty" bson:"official,omitempty"`
+			Common   string `json:"common,omitempty" bson:"common,omitempty"`
+		} `json:"zho,omitempty" bson:"zho,omitempty"`
+	} `json:"translations" bson:"translations"`
+	Latlng     []float64 `json:"latlng,omitempty" bson:"latlng,truncate,omitempty"`
+	Landlocked bool      `json:"landlocked,omitempty" bson:"landlocked,omitempty"`
+	Borders    []any     `json:"borders,omitempty" bson:"borders,omitempty"`
+	Area       int       `json:"area,omitempty" bson:"area,truncate,omitempty"`
+	Flag       string    `json:"flag,omitempty" bson:"flag,omitempty"`
 	Demonyms   struct {
 		Eng struct {
-			F string `bson:"f,omitempty"`
-			M string `bson:"m,omitempty"`
-		} `bson:"eng,omitempty"`
+			F string `json:"f,omitempty" bson:"f,omitempty"`
+			M string `json:"m,omitempty" bson:"m,omitempty"`
+		} `json:"eng,omitempty" bson:"eng,omitempty"`
 		Fra struct {
-			F string `bson:"f,omitempty"`
-			M string `bson:"m,omitempty"`
-		} `bson:"fra,omitempty"`
-	} `bson:"demonyms,omitempty"`
+			F string `json:"f,omitempty" bson:"f,omitempty"`
+			M string `json:"m,omitempty" bson:"m,omitempty"`
+		} `json:"fra,omitempty" bson:"fra,omitempty"`
+	} `json:"demonyms,omitempty" bson:"demonyms,omitempty"`
 	Flags struct {
-		Svg string `bson:"svg"`
-		Png string `bson:"png"`
-		Alt string `bson:"alt"`
-	} `bson:"flags"`
+		Svg string `json:"svg,omitempty" bson:"svg,omitempty"`
+		Png string `json:"png,omitempty" bson:"png,omitempty"`
+		Alt string `json:"alt,omitempty" bson:"alt,omitempty"`
+	} `json:"flags,omitempty" bson:"flags,omitempty"`
 	CoatOfArms struct {
-		Svg string `bson:"svg,omitempty"`
-		Png string `bson:"png,omitempty"`
-	} `bson:"coatOfArms,omitempty"`
-	Population int `bson:"population,truncate,omitempty"`
+		Svg string `json:"svg,omitempty" bson:"svg,omitempty"`
+		Png string `json:"png,omitempty" bson:"png,omitempty"`
+	} `json:"coatOfArms,omitempty" bson:"coatOfArms,omitempty"`
+	Population int `json:"population,omitempty" bson:"population,truncate,omitempty"`
 	Maps       struct {
 		GoogleMaps     string `bson:"googleMaps"`
 		OpenStreetMaps string `bson:"openStreetMaps"`
--- internals/server/handlers/countries.go
+++ internals/server/handlers/countries.go
@@ -7,65 +7,75 @@ import (
 	"github.com/ysrckr/countries-api/internals/db"
 	"github.com/ysrckr/countries-api/internals/models"
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func GetCountriesHandler(c fiber.Ctx) error {
-	countries := []models.Country{}
-	var cursor *mongo.Cursor
-	var err error
+
 	queries, ok := getQueries(c)
 	if ok {
-		var fields []string
-		var languages []string
-		optionsList := bson.D{bson.E{Key: "_id", Value: 1}, bson.E{Key: "name.common", Value: 1}}
-
-		fieldsQuery := queries["fields"]
-		if fieldsQuery != "" {
-			fields = strings.Split(fieldsQuery, ",")
-		}
-		languagesQuery := queries["languages"]
-		if languagesQuery != "" {
-			languages = strings.Split(languagesQuery, ",")
-		}
-
-		for _, field := range fields {
-			optionsList = append(optionsList, bson.E{Key: field, Value: 1})
-		}
-
-		for _, language := range languages {
-			optionsList = append(optionsList, bson.E{Key: language, Value: 1})
-		}
-
-		opts := options.Find().SetProjection(optionsList)
-
-		cursor, err := db.DB.QueryAll(c.Context(), "countries", bson.D{}, opts)
-		if err != nil {
-			return c.JSON(fiber.Map{
-				"message": err,
-			})
-		}
-
-		defer cursor.Close(c.Context())
-
-		if err = cursor.All(c.Context(), &countries); err != nil {
-			return c.JSON(fiber.Map{
-				"message": err,
-			})
-		}
-
-		return c.JSON(countries)
-
+		return withQueries(c, queries)
 	}
 
-	cursor, err = db.DB.QueryAll(c.Context(), "countries", bson.D{}, nil)
+	countries := []models.Country{}
+
+	cursor, err := db.DB.QueryAll(c.Context(), "countries", bson.D{}, nil)
+	if err != nil {
+		return c.JSON(fiber.Map{
+			"message": err,
+		})
+	}
+
+	defer cursor.Close(c.Context())
+
 	if err = cursor.All(c.Context(), &countries); err != nil {
 		return c.JSON(fiber.Map{
 			"message": err,
 		})
 	}
+
+	return c.JSON(countries)
+}
+
+func withQueries(c fiber.Ctx, queries map[string]string) error {
+	countries := []models.QueryCountry{}
+	var fields []string
+	var languages []string
+	optionsList := bson.D{bson.E{Key: "name.common", Value: 1}}
+
+	fieldsQuery := queries["fields"]
+	if fieldsQuery != "" {
+		fields = strings.Split(fieldsQuery, ",")
+	}
+	languagesQuery := queries["languages"]
+	if languagesQuery != "" {
+		languages = strings.Split(languagesQuery, ",")
+	}
+
+	for _, field := range fields {
+		optionsList = append(optionsList, bson.E{Key: field, Value: 1})
+	}
+
+	for _, language := range languages {
+		optionsList = append(optionsList, bson.E{Key: "translations." + language + ".common", Value: 1})
+	}
+
+	opts := options.Find().SetProjection(optionsList)
+
+	cursor, err := db.DB.QueryAll(c.Context(), "countries", bson.D{}, opts)
+	if err != nil {
+		return c.JSON(fiber.Map{
+			"message": err,
+		})
+	}
+
 	defer cursor.Close(c.Context())
+
+	if err = cursor.All(c.Context(), &countries); err != nil {
+		return c.JSON(fiber.Map{
+			"message": err,
+		})
+	}
 
 	return c.JSON(countries)
 }
